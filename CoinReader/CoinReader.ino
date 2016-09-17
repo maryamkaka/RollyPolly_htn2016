a894bee52e92582f7425c67d0910e08cf5953ee5
Maryam K <mkaka139@gmail.com>
2016-09-17 11:27:51 -0400

    CoinReader detection implemented

---
+++ CoinReader/CoinReader.ino
@@ -0,0 +1,17 @@
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+int sensorValue = 1023;        // value read from the pot
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+}
+
+void loop() {
+  delay(100);
+  
+  sensorValue = analogRead(analogInPin);
+
+  if(sensorValue != 1023){
+    Serial.print("1");
+  }
+}
